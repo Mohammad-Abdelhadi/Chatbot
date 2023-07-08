5c52e8505ec1e00f65308aa3b892da32d411b3d1
Mohammad-Abdelhadi <mohammad.b.abdelhadi@gmail.com>
2023-07-09 00:37:44 +0300

    done all the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import ChatBot from "./ChatBot"
+function App() {
+  return (
+    <div className="App">
+    <ChatBot/>
+    </div>
+  );
+}
+
+export default App;
